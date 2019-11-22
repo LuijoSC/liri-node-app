c90eb6acb06a1cb2d38a1885b216085436a703ee
Luis Joel Caballero <luijo@outlook.com>
2019-11-22 15:48:14 -0600

    Included Bandsintown at concertSearch function Added songSearch function to search a song on Spotify and get: artist(s), song name, album and preview link.

--- liri.js
+++ liri.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
+var colors = require('colors');
 var keys = require("./keys.js");
-var spotify = keys.spotify;
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 // console.log(keys);
 var artist = process.argv.slice(2).join(" ");
+var song = process.argv.slice(2).join(" ");
 
-console.log(artist);
+//Functions test
+// concertSearch();
+// songSearch();
 
-var axios = require("axios");
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+function concertSearch(){
+    var axios = require("axios");
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
     function (response){
-        console.log("Venue: " + response.data[0].venue.name);
-        console.log("Venue Location: " + response.data[0].venue.country + ", " + response.data[0].venue.city + ", " + response.data[0].venue.region);
-        console.log("Date: " + response.data[0].datetime)
+        for (i= 0; i < response.data.length; i++){
+            console.log("Venue: " + response.data[i].venue.name);
+            console.log("Venue Location: " + response.data[i].venue.country + ", " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+            console.log("Date: " + response.data[i].datetime);
+            console.log("===========================================================".green)
+        }
     })
     .catch(function(error){
         if (error.response){
@@ -29,3 +38,19 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
         }
         console.log(error.config)
     });
+};
+
+function songSearch(){
+spotify
+.search({ type: 'track', query: song, limit: 1})
+.then(function(response){
+    // console.log(response.tracks.items);
+    console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
+    console.log("Title: " + response.tracks.items[0].name);
+    console.log("Album: " + response.tracks.items[0].album.name);
+    console.log("Preview link: " + response.tracks.items[0].preview_url)
+})
+.catch (function(err){
+    console.log(err);
+});
+};
